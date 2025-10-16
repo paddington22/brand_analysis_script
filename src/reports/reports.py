18aa0af1e2b4ca5cf5835222e913a13c02d3d868
paddington22 <pkorolev23@gmail.com>
2025-10-16 14:37:47 +0300

    add reports

---
+++ src/reports/reports.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from tabulate import tabulate
+
+
+class BaseReport(ABC):
+    @abstractmethod
+    def generate(self, data):
+        pass
+
+    @abstractmethod
+    def display(self, result):
+        pass
+
+
+class AverageRatingReport:
+    def generate(self, data: list[dict[str, str]]) -> list[tuple]:
+        brand_ratings = {}
+        for row in data:
+            brand = row.get("brand")
+            rating = row.get("rating")
+
+            if brand not in brand_ratings:
+                brand_ratings[brand] = []
+            brand_ratings[brand].append(float(rating))
+
+        result = []
+        for brand, ratings in brand_ratings.items():
+            avg_rating = sum(ratings) / len(ratings)
+            result.append((brand, round(avg_rating, 2)))
+
+        result.sort(key=lambda x: x[1], reverse=True)
+
+        return result
+
+    def display(self, result: list[tuple]) -> None:
+        headers = ["", "brand", "rating"]
+        table_data = [
+            [index, *values] for index, values in enumerate(result, 1)
+        ]
+
+        print(
+            tabulate(
+                table_data, headers=headers, tablefmt="grid", stralign="left"
+            )
+        )
